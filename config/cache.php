0abdaddc60cd70be972fdc1f6245835d870aea76
Mads Møller <mm@napp.dk>
2020-05-10 19:16:35 +0200

    making sure dynamodb works

--- config/cache.php
+++ config/cache.php
@@ -78,7 +78,7 @@ return [
         ],
 
         'dynamodb' => [
-            'driver' => 'dynamodb',
+            'driver' => 'async-aws-dynamodb',
             'key' => env('AWS_ACCESS_KEY_ID'),
             'secret' => env('AWS_SECRET_ACCESS_KEY'),
             'region' => env('AWS_DEFAULT_REGION', 'us-east-1'),
